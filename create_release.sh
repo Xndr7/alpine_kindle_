82a4323a10755212aeed8fe134bf2e572d6a12b7
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-01 19:38:24 +0530

    Update create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rm alpine.zip
-apt install qemu-user-static
+apt-get install qemu-user-static
 bash create_kindle_alpine_image.sh
 zip alpine.zip alpine.conf alpine.sh alpine.ext3

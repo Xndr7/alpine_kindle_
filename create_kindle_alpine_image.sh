0585ca35dbb055f178f0b7ee87144a08240bd9ae
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-01 19:54:26 +0530

    Update create_kindle_alpine_image.sh

--- create_kindle_alpine_image.sh
+++ create_kindle_alpine_image.sh
@@ -26,11 +26,6 @@ apk update
 apk upgrade
 cat /etc/alpine-release
 apk add openssh vim curl nano git wget tmux net-tools iproute2 sudo bash
-apk add xorg-server-xephyr xwininfo xdotool xinput dbus-x11 
-apk add desktop-file-utils gtk-engines consolekit gtk-murrine-engine caja caja-extensions marco gnome-themes-extra
-apk add \$(apk search mate -q | grep -v '\-dev' | grep -v '\-lang' | grep -v '\-doc')
-apk add \$(apk search -q ttf- | grep -v '\-doc')
-apk add onboard chromium
 adduser alpine -D
 echo -e \"alpine\nalpine\" | passwd alpine
 echo '%sudo ALL=(ALL) ALL' >> /etc/sudoers

f8526783c411f6b1c1f7f853772918c886d45214
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-02 03:07:13 +0530

    Update create_kindle_alpine_image.sh

--- create_kindle_alpine_image.sh
+++ create_kindle_alpine_image.sh
@@ -132,10 +132,14 @@ chmod +x "$MNT/startgui.sh"
 cp $(which qemu-arm-static) "$MNT/usr/bin/"
 # Chroot and run the setup as specified at the beginning of the script
 #echo "Chrooting into Alpine"
-ALP_VER="(sudo cat /etc/alpine-release)"
-ALPINE_VERSION=(chroot /mnt/alpine/ qemu-arm-static /bin/sh -c "($ALPINESETUP > /dev/null 2>&1) && $ALP_VER") 
+#ALP_VER="(sudo cat /etc/alpine-release)"
+chroot /mnt/alpine/ qemu-arm-static /bin/sh -c "$ALPINESETUP"
+ALPINE_VERSION=$(sudo chroot /mnt/alpine cat /etc/alpine-release)
+
+# Print the Alpine version to the host
+echo "Alpine Version inside chroot: $ALPINE_VERSION"
 # Remove the qemu-arm-static binary again, it's not needed on the kindle
-echo "ALPINE_VERSION=$ALPINE_VERSION"
+#echo "ALPINE_VERSION=$ALPINE_VERSION"
 rm "$MNT/usr/bin/qemu-arm-static"
  
 # UNMOUNT IMAGE & CLEANUP
